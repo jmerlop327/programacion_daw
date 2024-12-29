b36f7d7a721a7855671aa1357a045a5850954ba4
juanfran mera (jmerlop327) <jmerlop327@g.educaand.es>
2024-12-29 19:45:00 +0100

    ejercicios de retos

--- proyecto_programacion/prog_daw/src/unidad04/boletin/ComidaPollitos328.java
+++ proyecto_programacion/prog_daw/src/unidad04/boletin/ComidaPollitos328.java
@@ -12,60 +12,146 @@ public class ComidaPollitos328 {
 		// in.next*()
 		int filas = in.nextInt();
 		int cols = in.nextInt();
-		int[][] corral = new int[filas][cols];
 		int pollitos = in.nextInt();
+		int[][] corral = new int[filas][cols];
+		in.nextLine();
 		for (int p = 0; p < pollitos; p++) {
-			in.nextLine();
 			String comportamientoPollito = in.nextLine();
 			String[] datosPollito = comportamientoPollito.split(" ");
 			int fila = Integer.parseInt(datosPollito[0]) - 1;
 			int col = Integer.parseInt(datosPollito[1]) - 1;
 			String direccion = datosPollito[2];
 			int pasos = Integer.parseInt(datosPollito[3]);
+			int i = fila, j = col;
+			int factor = 1;
+			int limite = 1;
+			corral[fila][col]++;
+			boolean chocado = false;
 
 			switch (direccion) {
 			case "N":
-				int i = fila, j = col;
-				int factor = 1;
-				int repeticiones = 0;
-				int limite = 1;
-				corral[i][j] = corral[i][j] + 1;
-				while (pasos > 0) {
-					repeticiones = 0;
-					while (repeticiones < limite && !chocado(corral, i, j)) {
-						i = i - factor;
-						corral[i][j] = corral[i][j] + 1;
+				while (pasos > 0 && !chocado) {
+					int repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						i -= factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
 						repeticiones++;
 					}
-					pasos--;
-					if (pasos > 0) {
-						j = j + factor;
-						corral[i][j] = corral[i][j] + 1;
+					repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						j += factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
 						pasos--;
+						repeticiones++;
 					}
-					factor = -(factor);
+					factor *= -1;
 					limite++;
 				}
 				break;
 			case "S":
-
+				while (pasos > 0 && !chocado) {
+					int repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						i += factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						j -= factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					factor *= -1;
+					limite++;
+				}
 				break;
 			case "E":
-
+				while (pasos > 0 && !chocado) {
+					int repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						j += factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						i += factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					factor *= -1;
+					limite++;
+				}
 				break;
 			case "W":
-
+				while (pasos > 0 && !chocado) {
+					int repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						j -= factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					repeticiones = 1;
+					while (pasos > 0 && !chocado && repeticiones <= limite) {
+						i -= factor;
+						chocado = chocado(corral, i, j);
+						if (pasos > 0 && !chocado) {
+							corral[i][j]++;
+						}
+						pasos--;
+						repeticiones++;
+					}
+					factor *= -1;
+					limite++;
+				}
 				break;
 			}
-			for (int i = 0; i < filas; i++) {
-				for (int j = 0; j < cols; j++) {
-					System.out.print(corral[i][j]+" ");
-				}
-				System.out.print("\n");
-			}
 		}
+		mostrarCorral(filas, cols, corral);
 
 	} // casoDePrueba
+
+	private static void mostrarCorral(int filas, int cols, int[][] corral) {
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (j < cols - 1) {					
+					System.out.print(corral[i][j] + " ");
+				} else {
+					System.out.print(corral[i][j]);
+				}
+			}
+			System.out.print("\n");
+		}
+		System.out.print("---\n");
+	}
 
 	private static boolean chocado(int[][] corral, int fila, int columna) {
 		boolean chocado = false;
--- proyecto_programacion/prog_daw/src/unidad04/boletin/Ej15TresEnRaya.java
+++ proyecto_programacion/prog_daw/src/unidad04/boletin/Ej15TresEnRaya.java
@@ -12,9 +12,8 @@ public class Ej15TresEnRaya {
 	final static String PLAYER_O = "O";
 
 	public static void main(String[] args) {
-		//TODO: Controlar la victoria de los jugadores 
-		//TODO: Controlar si hay espacios libres
-		//TODO: COntrolar si un usuario ha introducido una casilla ocupada (entradaCorrecta booleano)
+		//TODO: Permitir la jugada con numeroletra
+		// (entradaCorrecta booleano)
 		boolean ganaPlayer1 = false;
 		boolean ganaPlayer2 = false;
 		boolean hayHuecos = true;
@@ -26,35 +25,136 @@ public class Ej15TresEnRaya {
 		Arrays.fill(tablero[2], StringUtils.EMPTY);
 		System.out.println("Introduce una posición libre del tablero");
 		do {
-			mostrarTablero();
+			int huecosLibres = mostrarTablero();
+			int numPlayer = player.equals(PLAYER_X) ? 1 : 2;
+			System.out.printf("Turno del player %d\n", numPlayer);
 			System.out.println("Introduce una posición libre del tablero");
 			String jugada = scanner.nextLine();
 			int fila = 0;
 			int col = 0;
+			boolean casillaOcupada = false;
 			if (jugada.matches("[ABCabc][123]")) {
 				fila = Integer.parseInt(jugada.substring(1)) - 1;
 				String colString = jugada.substring(0, 1);
 				// colString.matches("[Aa]");
 				// StringUtils.equalsIgnoreCase(colString, "a");
-				if ("A".equals(colString) || "a".equals(colString)) {
-					col = 0;
-				} else if ("B".equals(colString) || "b".equals(colString)) {
-					col = 1;
-				} else if ("C".equals(colString) || "c".equals(colString)) {
-					col = 2;
-				}
+				col = getIndicePorLetra(colString);
 				if (StringUtils.EMPTY.equals(tablero[fila][col])) {
 					tablero[fila][col] = player;
+					int contadorCol = 0;
+					int contadorFila = 0;
+					int contadorDiaPrin = 0;
+					int contadorDiaSec = 0;
+
+					// compruebo la fila
+					for (int k = 0; k < tablero.length; k++) {
+						if (tablero[k][col] == player) {
+							contadorCol++;
+						}
+						if (tablero[fila][k] == player) {
+							contadorFila++;
+						}
+						// estamos en la diagonal principal
+						if (fila == col) {
+							if (tablero[k][k] == player) {
+								contadorDiaPrin++;
+							}
+						}
+						// estamos en la diagonal secundaria
+						if (fila == tablero[0].length - col - 1) {
+							if (tablero[k][tablero[0].length - k - 1] == player) {
+								contadorDiaSec++;
+							}
+						}
+					}
+					if (contadorFila == 3 || contadorCol == 3 || contadorDiaPrin == 3 || contadorDiaSec == 3) {
+						if (PLAYER_X.equals(player)) {
+							ganaPlayer1 = true;
+						} else {
+							ganaPlayer2 = true;
+						}
+					} else {
+						hayHuecos = (huecosLibres - 1 == 0) ? false : true;
+					}
+				} else {
+					// El jugador ha introducido un valor correcto pero dicha casilla está ocupada
+					casillaOcupada = true;
 				}
 			} else if (jugada.matches("[123][ABCabc]")) {
+				fila = Integer.parseInt(jugada.substring(1)) - 1;
+				String colString = jugada.substring(0, 1);
+				// colString.matches("[Aa]");
+				// StringUtils.equalsIgnoreCase(colString, "a");
+				col = getIndicePorLetra(colString);
+				if (StringUtils.EMPTY.equals(tablero[fila][col])) {
+					tablero[fila][col] = player;
+					int contadorCol = 0;
+					int contadorFila = 0;
+					int contadorDiaPrin = 0;
+					int contadorDiaSec = 0;
 
+					// compruebo la fila
+					for (int k = 0; k < tablero.length; k++) {
+						if (tablero[k][col] == player) {
+							contadorCol++;
+						}
+						if (tablero[fila][k] == player) {
+							contadorFila++;
+						}
+						// estamos en la diagonal principal
+						if (fila == col) {
+							if (tablero[k][k] == player) {
+								contadorDiaPrin++;
+							}
+						}
+						// estamos en la diagonal secundaria
+						if (fila == tablero[0].length - col - 1) {
+							if (tablero[k][tablero[0].length - k - 1] == player) {
+								contadorDiaSec++;
+							}
+						}
+					}
+					if (contadorFila == 3 || contadorCol == 3 || contadorDiaPrin == 3 || contadorDiaSec == 3) {
+						if (PLAYER_X.equals(player)) {
+							ganaPlayer1 = true;
+						} else {
+							ganaPlayer2 = true;
+						}
+					} else {
+						hayHuecos = (huecosLibres - 1 == 0) ? false : true;
+					}
+				} else {
+					// El jugador ha introducido un valor correcto pero dicha casilla está ocupada
+					casillaOcupada = true;
+				}
 			} else {
-				System.err
-						.println("Entrada inválida, debe introducir una jugada como \"letranumero\" o \"numeroletra\"");
+				System.err.println(
+						"Entrada inválida, debe introducir una jugada como \"[letra][numero]\" o \"[numero][letra]\"");
 			}
-			player = cambioJugador(player);
+			if (casillaOcupada) {
+				System.err.println("La casilla introducida está ocupada.");
+			} else if (ganaPlayer1) {
+				System.out.println("Enhorabuena player 1. Has ganado!");
+			} else if (ganaPlayer2) {
+				System.out.println("Enhorabuena player 2. Has ganado!");
+			} else {
+				player = cambioJugador(player);
+			}
 		} while (!ganaPlayer1 && !ganaPlayer2 && hayHuecos);
+		System.out.println("Hasta Luego!");
+		
+	}
 
+	private static int getIndicePorLetra(String colString) {
+		int col = 0;
+		if ("A".equals(colString) || "a".equals(colString)) {
+			col = 0;
+		} else if ("B".equals(colString) || "b".equals(colString)) {
+			col = 1;
+		} else if ("C".equals(colString) || "c".equals(colString)) {
+			col = 2;
+		}
+		return col;
 	}
 
 	/**
@@ -75,19 +175,25 @@ public class Ej15TresEnRaya {
 	/**
 	 * Muestra
 	 */
-	private static void mostrarTablero() {
+	private static int mostrarTablero() {
+		int huecosLibres = 0;
+		System.out.println("  a\t b\t c");
 		for (int i = 0; i < tablero.length; i++) {
-			System.out.print("|");
+			System.out.print(i + 1 + "|");
 			for (int j = 0; j < tablero[0].length; j++) {
 				if (j == tablero[0].length - 1) {
 					System.out.print(tablero[i][j]);
 				} else {
 					System.out.print(tablero[i][j] + "\t");
 				}
+				if (StringUtils.EMPTY.equals(tablero[i][j])) {
+					huecosLibres++;
+				}
 			}
 			System.out.print("|");
 			System.out.print("\n");
 		}
+		return huecosLibres;
 	}
 
 }
