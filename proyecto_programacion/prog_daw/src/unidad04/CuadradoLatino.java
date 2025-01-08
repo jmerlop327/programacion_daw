9ef904b8cf3b0681312654def690884b19c59f2f
juanfran mera (jmerlop327) <jmerlop327@g.educaand.es>
2025-01-08 19:14:35 +0100

    ejercicio Cuadrado Latino

---
+++ proyecto_programacion/prog_daw/src/unidad04/CuadradoLatino.java
@@ -0,0 +1,27 @@
+package unidad04;
+
+/**
+ * Crear un cuadrado latino en Java
+ * 
+ * Un cuadrado latino de orden N es una matriz cuadrada de N filas y N columnas
+ * en la que su primera fila contiene los N primeros números naturales y cada
+ * una de las filas siguientes contiene la rotación de la fila anterior un lugar
+ * a la derecha.
+ * 
+ * Por ejemplo, un cuadrado latino de orden 4 es una matriz cuadrada de tamaño 4
+ * x 4 con el siguiente contenido:
+ *
+ * 
+ * 1 2 3 4
+ * 4 1 2 3
+ * 3 4 1 2
+ * 2 3 4 1
+ */
+public class CuadradoLatino {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
