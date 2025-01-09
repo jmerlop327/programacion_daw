36741fde0a131f98c511a204a89e028c36c04a28
juanfran mera (jmerlop327) <jmerlop327@g.educaand.es>
2025-01-09 20:42:19 +0100

    TransponerMatriz

--- proyecto_programacion/prog_daw/src/unidad04/TransponerMatriz.java
+++ proyecto_programacion/prog_daw/src/unidad04/TransponerMatriz.java
@@ -1,5 +1,9 @@
 package unidad04;
 
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Dada una matriz A su transpuesta AT se obtiene intercambiando las filas por columnas y las columnas por filas. 
  * Por ejemplo, si tenemos la matriz A de 2 filas y 4 columnas:
@@ -18,8 +22,43 @@ package unidad04;
 public class TransponerMatriz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce los valores de la matriz para obtener la transpuesta");
+		System.out.println("Introduce el número de filas");
+		int filas = scanner.nextInt();
+		System.out.println("Introduce el número de columnas");
+		int cols = scanner.nextInt();
+		int[][] matriz = new int[filas][cols];
+		//inicializar la matriz con los datos
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				System.out.printf("Introduce el valor para la posición %d %d", i, j);
+				matriz[i][j] = scanner.nextInt();
+			}
+		}
+		System.out.println("Matriz inicial");
+		mostrarMatriz(matriz);
+		int[][] matrizT = obtenerMatrizTranspuesta(matriz);
+		System.out.println("Matriz transpuesta");
+		mostrarMatriz(matrizT);
 	}
 
+	private static int[][] obtenerMatrizTranspuesta(int[][] matriz) {
+		int[][] matrizT = new int[matriz[0].length][matriz.length];
+		for (int i = 0; i < matrizT.length; i++) {
+			for (int j = 0; j < matrizT[0].length; j++) {
+				matrizT[i][j] = matriz[j][i];
+			}
+		}
+		return matrizT;
+	}
+
+	private static void mostrarMatriz(int[][] matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				System.out.print(matriz[i][j] + "\t");
+			}
+			System.out.print("\n");
+		}
+	}
 }
