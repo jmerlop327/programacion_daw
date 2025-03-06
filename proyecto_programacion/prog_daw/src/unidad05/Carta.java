a8709b1d7646a16c19e94d9674078c279ba643d9
jmerlop327 <jmerlop327@g.educaand.es>
2025-03-06 19:02:00 +0100

    Carta con comparable

--- proyecto_programacion/prog_daw/src/unidad05/Carta.java
+++ proyecto_programacion/prog_daw/src/unidad05/Carta.java
@@ -1,11 +1,12 @@
 package unidad05;
 
+import java.util.Comparator;
 import java.util.Objects;
 
-public class Carta {
+public class Carta implements Comparable<Carta>, Comparator<Carta> {
 	private static final String[] ORDEN_PALOS = { "BASTOS", "ESPADAS", "COPAS", "OROS" };
-	private static final String[] ORDEN_NUMEROS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-	
+	private static final String[] ORDEN_NUMEROS = { "1", "2", "3", "4", "5", "6", "7", "10", "11", "12" };
+
 	// Atributos
 	private String palo;
 	private String numero;
@@ -56,10 +57,9 @@ public class Carta {
 	}
 
 	/**
-	 * Devuelve la carta mayor entre la carta pasada por parámetro y la actual
-	 * orden establecido:
-	 * oros > copas > espadas > bastos
-	 * A > 2 > 3 > 4 > 5 > ... 10 > J > Q > K
+	 * Devuelve la carta mayor entre la carta pasada por parámetro y la actual orden
+	 * establecido: oros > copas > espadas > bastos A > 2 > 3 > 4 > 5 > ... 10 > J >
+	 * Q > K
 	 * 
 	 * @param carta
 	 * @return cartaMayor
@@ -78,6 +78,23 @@ public class Carta {
 		return mayorCarta;
 	}
 
+	/**
+	 * Devuelve la carta mayor entre la carta pasada por parámetro y la actual orden
+	 * establecido: oros > copas > espadas > bastos A > 2 > 3 > 4 > 5 > ... 10 > J >
+	 * Q > K
+	 * 
+	 * @param carta
+	 * @return cartaMayor
+	 */
+	public Carta mayorCartaComparable(Carta carta) {
+		Carta mayorCarta = this;
+		// utilizar el compareTo
+		if (mayorCarta.compareTo(carta) < 0) {
+			mayorCarta = carta;
+		}
+		return mayorCarta;
+	}
+
 	private int calculaIndice(String[] orden, String dato) {
 		boolean encontrado = false;
 		int indice = 0;
@@ -92,5 +109,61 @@ public class Carta {
 		return indice;
 	}
 
-	
+	@Override
+	public int compareTo(Carta o) {
+		int compare = 0;
+		if (this.palo.equalsIgnoreCase(o.palo)) {
+			compare = Integer.parseInt(this.numero) - Integer.parseInt(o.numero);
+		} else if (this.palo.equalsIgnoreCase("BASTOS")) {
+			compare = 1;
+		} else if (this.palo.equalsIgnoreCase("ESPADAS") && !o.palo.equalsIgnoreCase("BASTOS")) {
+			compare = 1;
+		} else if (this.palo.equalsIgnoreCase("COPAS") && !o.palo.equalsIgnoreCase("BASTOS")
+				&& !o.palo.equalsIgnoreCase("ESPADAS")) {
+			compare = 1;
+		} else {
+			compare = -1;
+		}
+		return compare;
+	}
+
+	@Override
+	public int compare(Carta o1, Carta o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+
+	public int comparePorNumero(Carta o1, Carta o2) {
+		Integer numero1 = Integer.parseInt(o1.getNumero());
+		Integer numero2 = Integer.parseInt(o2.getNumero());
+		return numero1 - numero2;
+	}
+
+	public int comparePorPalo(Carta o1, Carta o2) {
+		int paloO1 = o1.getPaloLevel();
+		int paloO2 = o2.getPaloLevel();
+		return paloO1 - paloO2;
+	}
+
+	private int getPaloLevel() {
+		int level = 0;
+		switch (this.palo.toUpperCase()) {
+		case "OROS":
+			level = 1;
+			break;
+		case "ESPADAS":
+			level = 3;
+			break;
+		case "COPAS":
+			level = 2;
+			break;
+		case "BASTOS":
+			level = 4;
+			break;
+		default:
+			break;
+		}
+		return level;
+	}
+
 }
---
+++ proyecto_programacion/prog_daw/src/unidad05/CartaMainComparable.java
@@ -0,0 +1,79 @@
+package unidad05;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class CartaMainComparable {
+
+	public static void main(String[] args) {
+		Carta asEspadas = new Carta("Espadas", "1");
+		Carta dosEspadas = new Carta("Espadas", "2");
+		Carta carta1 = new Carta("Espadas", "3");
+		Carta carta2 = new Carta("Bastos", "1");
+		Carta carta4 = new Carta("Espadas", "11");
+		Carta carta3 = new Carta("copas", "10");
+		Carta carta5 = new Carta("oros", "7");
+		Carta carta6 = new Carta("Espadas", "10");
+		//Carta cartaMayor = asEspadas.mayorCarta(cartaVacia);
+		//System.out.println("la mayor carta de " + asEspadas + " y " + cartaVacia + " es " + cartaMayor);
+		Carta[] cartas = { asEspadas, dosEspadas, carta1, carta2, carta3, carta4, carta5, carta6 };
+		System.out.println("antes");
+		mostrarCartas(cartas);
+//		System.out.println("despues");
+//		Arrays.sort(cartas);
+//		mostrarCartas(cartas);
+		
+		Carta cartaCompare = new Carta();
+		Comparator<Carta> comparatorPorNumero = cartaCompare::comparePorNumero;
+		Arrays.sort(cartas, comparatorPorNumero);
+		System.out.println("despues NUM");
+		mostrarCartas(cartas);
+		Comparator<Carta> comparatorPorPalo = cartaCompare::comparePorPalo;
+		Arrays.sort(cartas, comparatorPorPalo);
+		System.out.println("despues PALO");
+		mostrarCartas(cartas);
+		
+	}
+
+	private static void mostrarCartas(Carta[] cartas) {
+		for (Carta carta : cartas) {
+			System.out.println(carta);
+		}
+	}
+	
+
+	public static void ordenarCartas (Carta[] cartas) {
+		// Método de ordenación de la burbuja
+				boolean intercambio = true;
+				int elementosPorRecorrer = cartas.length-1;
+				while (intercambio) {
+					intercambio = false;
+					for (int i = 0;i<elementosPorRecorrer;i++) {
+						// si la carta de la izq es menor que la de la derecha las tengo que cambiar
+						Carta cartaIzq = cartas[i]; 
+						Carta cartaDch = cartas[i+1];
+						Carta mayor = cartaIzq.mayorCartaComparable(cartaDch);
+						if (mayor.equals(cartaIzq)) {
+							// si están desordenados intercambiamos los elementos adyacentes
+							Carta aux = cartaIzq;
+							cartas[i] = cartaDch;
+							cartas[i+1] = aux;
+							intercambio = true;
+						}
+					}
+					elementosPorRecorrer--;
+				}
+	}
+	
+	public static void mezclarCartas(Carta[] cartas) {
+		Random rand = new Random();
+		for (int i = cartas.length - 1; i > 0; i--) {
+			int aleatorio = rand.nextInt(i+1);
+			Carta aux = cartas[i];
+			cartas[i] = cartas[aleatorio];
+			cartas[aleatorio] = aux;
+		}
+	}
+
+}
