629a0a6f4209236624a2b1c865361a63f7850959
jmerlop327 <jmerlop327@g.educaand.es>
2025-05-14 20:41:20 +0200

    ejercicios finales ficheros

--- proyecto_programacion/prog_daw/src/unidad07/ampliacion/Gestisimal.java
+++ proyecto_programacion/prog_daw/src/unidad07/ampliacion/Gestisimal.java
@@ -1,37 +1,49 @@
 package unidad07.ampliacion;
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Gestisimal {
 	static Almacen alm = new Almacen();
-	static Set<Producto> productos = alm.getProductos(); 
+	static Set<Producto> productos = alm.getProductos();
+
 	public static void main(String[] args) {
 		boolean salir = false;
 		do {
 			mostrarMenu();
 			Scanner scan = new Scanner(System.in);
-			//obtener opcion
-			int opc = Integer.parseInt(scan .nextLine());
+			// obtener opcion
+			int opc = Integer.parseInt(scan.nextLine());
 			switch (opc) {
 			case 1:
-				//mostrar listado de productos
+				// mostrar listado de productos
 				break;
 			case 2:
-				//Dar de baja un producto por su codigo
+				// Dar de baja un producto por su codigo
 				break;
 			case 3:
-				//Búsqueda de productos
+				// Búsqueda de productos
 				break;
 			case 4:
-				//Modificar pvp por su codigo
+				// Modificar pvp por su codigo
 				break;
 			case 5:
 				// Actualización del almacen por fichero
+				System.out.println("Introduce la ruta del fichero:");
+				String ruta = scan.nextLine();
+				File fichero = new File("ficheros/gestisimal/" + ruta);
+				if (!fichero.exists()) {
+					System.err.printf("El fichero %s no se encuentra en el sistema\n", ruta);
+				} else {
+					alm.actualizarAlmacen(fichero);
+				}
 				break;
 			case 6:
-				salir = true;
 				// Guardar estado actual en un fichero YYYYMMDD_gestisimal.csv
+				String rutaEscritura = "ficheros20250414_gestisimal.csv";
+				alm.guardarDatosAlmacen(new File(rutaEscritura));
+				salir = true;
 				break;
 
 			default:
@@ -40,6 +52,7 @@ public class Gestisimal {
 			}
 		} while (!salir);
 	}
+
 	private static void mostrarMenu() {
 		// TODO Auto-generated method stub
 	}
---
+++ proyecto_programacion/prog_daw/src/unidad07/ampliacion/ProductoException.java
@@ -0,0 +1,14 @@
+package unidad07.ampliacion;
+
+public class ProductoException extends Exception {
+
+	public ProductoException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3797247180642304372L;
+
+}
